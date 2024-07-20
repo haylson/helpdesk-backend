974ea06266072abc139484c3bcee6f13898569df
haylson <haylson@gmail.com>
2024-07-20 01:36:30 -0300

    Exceção para CPF e EMAIL já cadastrados

--- src/main/java/com/hm/helpdesk/repositories/PessoaRepository.java
+++ src/main/java/com/hm/helpdesk/repositories/PessoaRepository.java
@@ -1,9 +1,13 @@
 package com.hm.helpdesk.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hm.helpdesk.domain.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer>{
-
+	
+	Optional<Pessoa> findByCpf(String cpf);
+	Optional<Pessoa> findByEmail(String email);
 }
